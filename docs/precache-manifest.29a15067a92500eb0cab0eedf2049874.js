0cca022f1fd8e23c7ec0a9295d02329819611958
Irving_isc <irvingsalvador92@gmail.com>
2020-09-13 20:20:36 -0500

    docs agregado

---
+++ docs/precache-manifest.29a15067a92500eb0cab0eedf2049874.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d558fc9603504f807cb382249abd8512",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d4d03528f8bb34dc8e0",
+    "url": "/static/css/main.122d52ca.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d4d03528f8bb34dc8e0",
+    "url": "/static/js/main.d9f5c63f.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
